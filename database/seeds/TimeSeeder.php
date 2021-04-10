0eda2f4ea7cabf13fd16f12ea2a9a2fd0c5e48db
ArtARTs36 <temicska99@mail.ru>
2021-04-10 20:00:24 +0300

    fix time seeder

--- database/seeds/TimeSeeder.php
+++ database/seeds/TimeSeeder.php
@@ -1,17 +1,21 @@
 <?php
 
+use ArtARTs36\ControlTime\Contracts\SubjectRepository;
 use ArtARTs36\ControlTime\Models\Time;
+use ArtARTs36\ControlTime\Repositories\EmployeeRepository;
 use ArtARTs36\ControlTime\Support\Proxy;
 
 class TimeSeeder extends \Illuminate\Database\Seeder
 {
     public function run(): void
     {
-        $employees = app(\ArtARTs36\ControlTime\Repositories\EmployeeRepository::class)->all();
+        $employees = app(EmployeeRepository::class)->all();
+        $subjects = app(SubjectRepository::class)->all();
 
         foreach ($employees as $employee) {
-            factory(Time::class, 20)->make()->each(function (Time $time) use ($employee) {
+            factory(Time::class, 20)->make()->each(function (Time $time) use ($employee, $subjects) {
                 $time->employee_id = $employee->id;
+                $time->subject_id = $subjects->random()->id;
                 $time->save();
             });
         }
