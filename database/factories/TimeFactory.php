17bdcc8d7abefe523f94a05025694ba20ffc594e
ArtARTs36 <temicska99@mail.ru>
2021-04-16 00:28:49 +0300

    update time factory/seeder

--- database/factories/TimeFactory.php
+++ database/factories/TimeFactory.php
@@ -7,13 +7,13 @@ use ArtARTs36\ControlTime\Models\Time;
 use ArtARTs36\ControlTime\Tests\Prototypes\User;
 use Faker\Generator as Faker;
 
-$factory->define(Time::class, function (Faker $faker) {
+$factory->define(Time::class, function (Faker $faker, array $attributes = []) {
     return [
         Time::FIELD_DATE => $faker->dateTime()->format('Y-m-d'),
         Time::FIELD_QUANTITY => rand(0, Time::FULL_TIME),
         Time::FIELD_COMMENT => $faker->text(200),
-        Time::FIELD_SUBJECT_ID => factory(Subject::class)->create()->id,
-        Time::FIELD_EMPLOYEE_ID => factory(config(
+        Time::FIELD_SUBJECT_ID => $attributes[Time::FIELD_SUBJECT_ID] ?? factory(Subject::class)->create()->id,
+        Time::FIELD_EMPLOYEE_ID => $attributes[Time::FIELD_EMPLOYEE_ID] ?? factory(config(
             'controltime.employee.model_class'
         ))->create()->id,
     ];
--- database/seeds/TimeSeeder.php
+++ database/seeds/TimeSeeder.php
@@ -3,21 +3,25 @@
 use ArtARTs36\ControlTime\Contracts\SubjectRepository;
 use ArtARTs36\ControlTime\Models\Time;
 use ArtARTs36\ControlTime\Repositories\EmployeeRepository;
-use ArtARTs36\ControlTime\Support\Proxy;
+use Illuminate\Support\Collection;
 
 class TimeSeeder extends \Illuminate\Database\Seeder
 {
     public function run(): void
     {
+        /** @var \ArtARTs36\EmployeeInterfaces\Employee\EmployeeInterface[] $employees */
         $employees = app(EmployeeRepository::class)->all();
+
+        /** @var Collection|\ArtARTs36\ControlTime\Models\Subject[] $subjects */
         $subjects = app(SubjectRepository::class)->all();
 
         foreach ($employees as $employee) {
-            factory(Time::class, 20)->make()->each(function (Time $time) use ($employee, $subjects) {
-                $time->employee_id = $employee->id;
-                $time->subject_id = $subjects->random()->id;
-                $time->save();
-            });
+            for ($i = 0; $i < 20; $i++) {
+                factory(Time::class)->create([
+                    Time::FIELD_EMPLOYEE_ID => $employee->getId(),
+                    Time::FIELD_SUBJECT_ID => $subjects->random()->id,
+                ]);
+            }
         }
     }
 }
